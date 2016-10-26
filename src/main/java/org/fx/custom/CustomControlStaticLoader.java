596d7c2d1745dbef8f64ee9280cb59ceb290c8d7
Constantin Ungureanu <constantin.ungureanu-coun@asml.com>
2016-10-26 18:13:34 +0200

    Updated the loading.

--- src/main/java/org/fx/custom/CustomControlStaticLoader.java
+++ src/main/java/org/fx/custom/CustomControlStaticLoader.java
@@ -3,7 +3,6 @@ package org.fx.custom;
 import java.io.IOException;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 
 public final class CustomControlStaticLoader {
     private static final FXMLLoader fxmlLoader = new FXMLLoader();
@@ -11,11 +10,11 @@ public final class CustomControlStaticLoader {
     private CustomControlStaticLoader() {
     }
 
-    public static void load(final Node node, final String fxmlFile) {
+    public static <T> T load(final T node, final String fxmlFile) {
         try {
             fxmlLoader.setRoot(node);
             fxmlLoader.setController(node);
-            fxmlLoader.load(node.getClass().getResourceAsStream(fxmlFile));
+            return fxmlLoader.load(node.getClass().getResourceAsStream(fxmlFile));
         } catch (final IOException ioe) {
             throw new IllegalStateException("Cannot load FXML", ioe);
         }
